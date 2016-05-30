0dc33e1c5bab0a6cc6f7ae45692a599ffc1de35d
zain-jr <zainjrtech21@gmail.com>
2016-05-30 10:42:17 +0500

    overall changes

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,18 +1,3 @@
-// page init
-jQuery(function(){
-	initTabs();
-});
-
-// content tabs init
-function initTabs() {
-	jQuery('.tabset').contentTabs({
-		tabLinks: 'a',
-		effect: 'fade',
-		animSpeed:200,
-		switchTime:1000
-	});
-}
-
 $('.hidden-checkfield').change(function(){
     if($(this).is(":checked")) {
         $('.registration-form').addClass("agent-info");
